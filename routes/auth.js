802477acaef6ae10db46abec2201331067df1fd6
Wittawat Sangpong <wittawat.s@gipsic.com>
2022-02-09 18:11:55 +0700

    test login

--- routes/auth.js
+++ routes/auth.js
@@ -9,15 +9,23 @@ router.post('/', async (req,res) => {
             email: req.body.email,
             password: req.body.password,
         });
-        var result = await emp.find({email: e.email}).exec();
-
-                if(result[0].password === e.password)
+        var result = await emp.findOne({email: e.email}).exec();
+                if(result === null){
+                  res.send({'status' : "error",'detail' : 'ไม่พบ email'});
+                }
+                else if(result.password === e.password)
                 {
-                    var jwtSecret = await jwt.find({jwtId:'000001'}).exec()
-                    // res.send(result);
+                    var jwtSecret = await jwt.findOne({jwtId:'000001'}).exec()
+                    if(jwtSecret === null){
+                      res.send({'status' : "error",'detail' : 'password ไม่ถูกต้อง'});
+                    }
                     const token = jwtToken.sign(
-                        { email: result[0].email, position:result[0].position },
-                        jwtSecret[0].jwtSecret,
+                        { email: result.email, 
+                          first_name: result.first_name, 
+                          last_name: result.last_name, 
+                          role:result.role ,
+                          position:result.position },
+                        jwtSecret.jwtSecret,
                         {
                           expiresIn: "2h",
                         }
--- routes/emp.js
+++ routes/emp.js
@@ -1,8 +1,52 @@
 const express = require('express');
 const router = express.Router();
 const emp = require('../models/emp')
+const jwt = require('../models/jwt')
+const jwtToken = require('jsonwebtoken');
 
-router.get('/', async (req,res) => {
+const jwtSecret = async() =>{
+    var jwtSecret = await jwt.findOne({jwtId:'000001'}).exec()
+        if(jwtSecret === null){
+            return {'status' : "error",'detail' : 'ไม่พบ jwtSecret'};
+        }
+        return jwtSecret;
+}
+const checkLogin = async(req,res,next) =>{
+    // if(req.headers.xdes === "ton") {
+    //     req.token = {
+    //         fname:"ton",
+    //         role: "admin"
+    //     };
+    //     return next();
+    // } else {
+    //     return res.status(401).end();
+    // }
+    
+    // var decoded = jwtToken.verify(req.headers.token, jwtSecret());
+    const jwtSec = await jwtSecret();
+    try {
+        var decoded = jwtToken.verify(req.headers.token, jwtSec.jwtSecret);
+        req.user = {
+            email:decoded.email ,
+            first_name:decoded.first_name ,
+            last_name:decoded.last_name,
+            role:decoded.role ,
+            position:decoded.position ,
+
+        }
+      } catch(err) {
+        return res.status(401).end();
+      }
+    console.log("user", req.user)
+    next();
+}
+
+const checkAdmin = (req,res,next) =>{
+    console.log("checkAdmin", req.user.role)
+    next();
+}
+
+router.get('/',checkLogin, checkAdmin, async (req,res) => {
     try {
         var result = await emp.find({  });
         res.send(result);
